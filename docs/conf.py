63c33ba9a13ba5598c049d3e36f99114288ab69e
Matthew Watkins <mwatkins@linuxfoundation.org>
2026-03-09 18:23:56 +0000

    Docs: Modernise docs configuration for Python 3.13
    Replace lfdocs-conf dependency with standalone conf.py to eliminate the
    upstream blockdiag/seqdiag/swaggerdoc/pkg_resources dependency chain.
    The lfdocs-conf package imports sphinxcontrib-blockdiag which requires
    pkg_resources, removed from setuptools>=82.
    
    Changes:
    - conf.py: Replace 'from docs_conf.conf import *' with standalone ONAP
      configuration matching doc-best-practice reference; fix deprecated
      add_stylesheet -> add_css_file for Sphinx 8.x; add RTD context
      injection block
    - conf.yaml: Remove (no longer required without lfdocs-conf)
    - requirements-docs.txt: Remove lfdocs-conf and all blockdiag/seqdiag/
      swaggerdoc/nwdiag/needs/bootstrap dependencies; add modern Sphinx and
      sphinx-rtd-theme version bounds
    - tox.ini: Update whitelist_externals -> allowlist_externals for tox 4;
      add upper-constraints and setuptools dep; add -W -q flags to
      sphinx-build; add docs-linkcheck to envlist with proper deps
    - .readthedocs.yaml: Replace obsolete python 3.7 and build image latest
      with os ubuntu-24.04 and python 3.13; remove formats htmlzip
    - ribbon.css: Fix max-width from none to 800px per doc-rules
    - index.rst: Add toctree with cia-casablanca-release-notes to fix
      Sphinx -W warning about orphaned document
    
    Issue-ID: CIMAN-33
    Change-Id: I13565ab009fc3012df27c02d5a6a592c8a5c72ff
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,58 @@
-from docs_conf.conf import *
+project = "onap"
+release = "master"
+version = "master"
+
+#####
+# Deprecation of Sphinx context injection at build time
+# see https://about.readthedocs.com/blog/2024/07/addons-by-default/
+import os
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+#
+#####
+
+author = "Open Network Automation Platform"
+# yamllint disable-line rule:line-length
+copyright = "ONAP. Licensed under Creative Commons Attribution 4.0 International License"
+
+exclude_patterns = [
+    '.tox'
+]
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "style_nav_header_background": "white",
+    "sticky_navigation": "False" }
+html_logo = "_static/logo_onap_2017.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_show_sphinx = False
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.mermaid',
+    'sphinxcontrib.plantuml',
+]
 
 branch = 'latest'
+
+intersphinx_mapping = {}
 master_doc = 'index'
 
 linkcheck_ignore = [
     'http://localhost',
 ]
 
-intersphinx_mapping = {}
-
 html_last_updated_fmt = '%d-%b-%y %H:%M'
 
 def setup(app):
-    app.add_stylesheet("css/ribbon.css")
+    app.add_css_file("css/ribbon.css")
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,10 @@
 .. This work is licensed under a Creative Commons Attribution 4.0 International License.
 .. _master_index:
 
-Nothing Here
-------------
+ONAP Container Image Automation (CIA)
+=====================================
+
+.. toctree::
+   :maxdepth: 1
+
+   cia-casablanca-release-notes
